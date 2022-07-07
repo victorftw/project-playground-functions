556cdc41f5861ba28c7ad25b119e6e7457440ff7
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-07 12:43:48 -0300

    primeiro requisito bonus concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -80,9 +80,7 @@ function generatePhoneNumber(array) {
 generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck() {}
 
 // Desafio 13
 function hydrate() {
